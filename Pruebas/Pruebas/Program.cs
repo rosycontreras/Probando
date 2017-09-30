43c0a10b2053115a44a2aaae13c6ccbf622bbad9
Erick Contreras <ericon99sdb@gmail.com>
2017-09-30 12:29:48 -0600

    Probando como funciona

---
+++ Pruebas/Pruebas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 12;
+            double b = 5;
+            double r = a / b;
+            int r2 = int.Parse(a.ToString()) / Convert.ToInt32(b);
+            double r3 = a % b;
+
+            Console.WriteLine(r);
+            Console.WriteLine(r2);
+            Console.WriteLine(r3);
+            Console.ReadKey();
+        }
+    }
+}
